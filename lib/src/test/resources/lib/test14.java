ba5aa2915dc1913eb4eca684a07cda8d3b8d86b4
ruchir <ruchiragarwalcs@gmail.com>
2024-07-13 12:29:09 +0530

    Code changes - Ruchir

--- lib/src/test/resources/lib/test14.java
+++ lib/src/test/resources/lib/test14.java
@@ -10,8 +10,8 @@ public class test14 {
 	@Test(priority=1)
 	  public void h1()
 	  {
-		  System.out.println("Automation");
-		  commonfunctions.LogPass("Automation");
+		  System.out.println("Automation Testing");
+		  commonfunctions.LogPass("Automation Testing");
 		  
 	  }
 	
